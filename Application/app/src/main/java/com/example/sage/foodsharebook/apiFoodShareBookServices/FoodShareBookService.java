b08a11d53a24f47281af44aecae3ed1028576f43
German <german2506.gs@gmail.com>
2017-12-20 17:34:52 -0700

    inicio de POST

--- Application/app/src/main/java/com/example/sage/foodsharebook/apiFoodShareBookServices/FoodShareBookService.java
+++ Application/app/src/main/java/com/example/sage/foodsharebook/apiFoodShareBookServices/FoodShareBookService.java
@@ -6,7 +6,10 @@ import com.example.sage.foodsharebook.models.Ingredient;
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by Marisa on 20/12/2017.
@@ -18,4 +21,8 @@ public interface FoodShareBookService {
 
     @GET("ingredients")
     Call<ArrayList<Ingredient>> getAllIngredients();
+
+    @POST("ingredients")
+    @FormUrlEncoded
+    Call<Ingredient> newIngredient(@Body Ingredient body);
 }
