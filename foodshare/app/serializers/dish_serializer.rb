ac933056d58922405bf73f0f448df7c711c49323
Eduardo Salazar <german2506.gs@gmail.com>
2019-08-30 13:44:50 -0600

    removed Ruby api from master

--- foodshare/app/serializers/dish_serializer.rb
+++
@@ -1,8 +0,0 @@
-class DishSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :recipe, :ingredient_ids
-  # has_many :ingredients
-
-  def ingredient_ids
-    object.ingredients.map(&:id)
-  end
-end
--- foodshare/db/migrate/20171218150005_create_dish_ingredients.rb
+++
@@ -1,9 +0,0 @@
-class CreateDishIngredients < ActiveRecord::Migration[5.1]
-  def change
-    create_table :dish_ingredients do |t|
-      t.references :dish, index:true
-      t.references :ingredient, index:true
-      t.timestamps
-    end
-  end
-end
